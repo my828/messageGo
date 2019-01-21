f7d0d634125fcaebdc9e5420a6de8c99be0c2fca
Min Yang <minyang@Mins-MacBook-Pro.local>
2019-01-21 15:01:24 -0800

    new update

--- clients/summary/upgrade-server.sh
+++ clients/summary/upgrade-server.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 docker pull my828/summary2
 docker rm -f summary2
-export TLSCERT=/etc/letsencrypt/live/api.turtlemaster.me/fullchain.pem
-export TLSKEY=/etc/letsencrypt/live/api.turtlemaster.me/privkey.pem
 docker run -d -p 80:80 -p 443:443 -v /etc/letsencryt:/etc/letsencrypt:ro my828/summary2

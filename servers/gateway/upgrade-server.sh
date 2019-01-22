6e80fe2711e1a6d936ce6a647912bb5a53eb4d29
Min Yang <minyang@Mins-MacBook-Pro.local>
2019-01-21 19:12:55 -0800

    fixed spelling errorrrrr

--- servers/gateway/upgrade-server.sh
+++ servers/gateway/upgrade-server.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-docker pull my828/gateway
-docker rm -f gateway 
 export TLSCERT=/etc/letsencrypt/live/api.turtlemaster.me/fullchain.pem
 export TLSKEY=/etc/letsencrypt/live/api.turtlemaster.me/privkey.pem
-docker run -d --name gateway -p 443:443 -e TLSCERT=$TLSCERT -e TLSKEY=$TLSKEY  -v /etc/letsencryt:/etc/letsencrypt:ro my828/gateway
+
+docker rm -f gateway 
+docker pull my828/gateway
+
+
+docker run -d --name gateway -p 443:443 -e TLSCERT=$TLSCERT -e TLSKEY=$TLSKEY  -v /etc/letsencrypt:/etc/letsencrypt:ro my828/gateway

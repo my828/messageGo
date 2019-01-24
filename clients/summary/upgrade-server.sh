6e3ff1f1c4ee6fd224117379810d0486cf5c2e7b
my828 <minyang@Mins-MacBook-Pro.local>
2019-01-24 09:32:07 -0800

    update

--- clients/summary/upgrade-server.sh
+++ clients/summary/upgrade-server.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-docker pull my828/summary2
+
 docker rm -f summary2
-docker run -d -p 80:80 -p 443:443 -v /etc/letsencryt:/etc/letsencrypt:ro my828/summary2
+docker pull my828/summary2
+
+docker run -d -p 80:80 -p 443:443 -v /etc/letsencrypt:/etc/letsencrypt:ro my828/summary2

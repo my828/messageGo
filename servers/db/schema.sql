1cf346b1f2d77f9e9cf56b53b1c8455c1a1a0065
my828 <minyang828@gmail.com>
2019-02-07 21:42:36 -0800

    first commit

--- servers/db/schema.sql
+++ servers/db/schema.sql
@@ -1,10 +1,17 @@
 create table if not exists users (
     id int not null auto_increment primary key,
     email varchar(255) not null,
-    pass_hash varchar(64) not null,
-    user_name varchar(255) not null,
-    first_name varchar(64) not null,
-    last_name varchar(128) not null,
-    photo_url varchar(128) not null,
-    unique key(email, user_name)
-)
+    passHash binary(60) not null,
+    userName varchar(255) not null,
+    firstName varchar(64) not null,
+    lastName varchar(128) not null,
+    photoUrl varchar(128) not null,
+    unique key(email, userName)
+);
+
+create table if not exists signinuser (
+    id2 int not null auto_increment primary key,
+    userID int not null,
+    signinDatetime datetime not null,
+    ipAddress varchar(50) not null
+);

3590addaf813cf8be43f393cecb864487ea45f4d
my828 <minyang828@gmail.com>
2019-02-11 17:33:23 -0800

    changes

--- servers/gateway/indexes/trie.go
+++ servers/gateway/indexes/trie.go
@@ -2,3 +2,79 @@ package indexes
 
 //TODO: implement a trie data structure that stores
 //keys of type string and values of type int64
+
+//PRO TIP: if you are having troubles and want to see
+//what your trie structure looks like at various points,
+//either use the debugger, or try this package:
+//https://github.com/davecgh/go-spew
+
+//Trie implements a trie data structure mapping strings to int64s
+//that is safe for concurrent use.
+type Node struct {
+	val int64set
+	endOfWord bool
+	children map[rune]*Node
+}
+
+type Trie struct {
+	root *Node
+	size int
+}
+
+//NewTrie constructs a new Trie.
+func NewTrie() *Trie {
+	return &Trie{}
+}
+
+//Len returns the number of entries in the trie.
+func (t *Trie) Len() int {
+	panic("implement this function according to the comments above")
+}
+
+//Add adds a key and value to the trie.
+func (t *Trie) Add(key string, value int64) {
+	panic("implement this function according to the comments above")
+}
+
+//Find finds `max` values matching `prefix`. If the trie
+//is entirely empty, or the prefix is empty, or max == 0,
+//or the prefix is not found, this returns a nil slice.
+func (t *Trie) Find(prefix string, max int) []int64 {
+	panic("implement this function according to the comments above")
+}
+
+//Remove removes a key/value pair from the trie
+//and trims branches with no values.
+func (t *Trie) Remove(key string, value int64) {
+	panic("implement this function according to the comments above")
+}
+
+//int64set is a set of int64 values
+type int64set map[int64]struct{}
+
+//add adds a value to the set and returns
+//true if the value didn't already exist in the set.
+func (s int64set) add(value int64) bool {
+	if !s.has(value) {
+		s[value] = struct{}{}
+	}
+	return s.has(value)
+}
+
+//remove removes a value from the set and returns
+//true if that value was in the set, false otherwise.
+func (s int64set) remove(value int64) bool {
+}
+
+//has returns true if value is in the set,
+//or false if it is not in the set.
+func (s int64set) has(value int64) bool {
+	
+}
+
+//all returns all values in the set as a slice.
+//The returned slice will always be non-nil, but
+//the order will be random. Use sort.Slice to
+//sort the slice if necessary.
+func (s int64set) all() []int64 {
+}

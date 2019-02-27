59989d6b35d8fc4d1b255887e392bb82d6d8d7bc
my828 <minyang828@gmail.com>
2019-02-26 22:16:10 -0800

    commit

--- servers/messaging/api/models/channel.js
+++ servers/messaging/api/models/channel.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const channelSchema = mongoose.Schema({
     name: {
         type: String,
+        unique: true,
         required: [true, 'Name field is required']
     },
     description: {
